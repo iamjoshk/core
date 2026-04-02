b3043e3c920ded7636d1af401811175a568f8943
Josh K <28068117+iamjoshk@users.noreply.github.com>
2026-04-02 11:08:38 -0400

    fix options flow

--- homeassistant/components/schlage/config_flow.py
+++ homeassistant/components/schlage/config_flow.py
@@ -11,6 +11,7 @@ import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult, OptionsFlow
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import callback
 from homeassistant.helpers import selector
 
 from .const import (
@@ -32,7 +33,13 @@ class SchlageConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Schlage."""
 
     VERSION = 1
-
+    
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return SchlageOptionsFlowHandler()
+    
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
